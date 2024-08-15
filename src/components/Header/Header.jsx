c031e9d8a1c29e7fae93dd4a482238e9c6a6ab62
Mahik <mahik09@outlook.com>
2024-08-15 15:04:02 +0100

    updated menu CSS

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,27 @@
-'use client';
-import styles from './Header.module.css'
-import Image from 'next/image';
+"use client";
+import { useState } from "react";
+import styles from "./Header.module.css";
+import Image from "next/image";
+import Menu from "../Menu/Menu";
 
 const Header = () => {
-  function handleClick()
-  {
-    console.log("Button clicked");
+  const [menuStatus, setMenuStatus] = useState(false);
+  function handleClick() {
+    setMenuStatus(true);
+  }
+  function handleCloseClick() {
+    setMenuStatus(false);
   }
   return (
     <header className={styles.headerContainer}>
       <h1 className={styles.title}>🔥 Fireplace Palace</h1>
-      <button type="button" onClick={handleClick}><Image src="/menu-open-button.png" width="25" height="25"></Image></button>
+      <button type="button" onClick={handleClick}>
+        <Image src="/menu-open-button.png" width="25" height="25"></Image>
+      </button>
+      {menuStatus && <Menu></Menu>}
+      <button type="button" onClick={handleCloseClick}>
+        <Image src="/menu-close-button.png" width="25" height="25"></Image>
+      </button>
     </header>
   );
 };
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,11 @@
-import Link from 'next/link'
-import styles from './Menu.module.css'
+import Link from "next/link";
+import styles from "./Menu.module.css";
 
-export default function Menu(){
-    return(
-    <div className = {styles.div}>
-        <Link href="/home">Home</Link>
-        <Link href="/founders">Founders</Link>
+export default function Menu() {
+  return (
+    <div className={styles.div}>
+      <Link href="/">Home</Link>
+      <Link href="/founders">Founders</Link>
     </div>
-    )
+  );
 }
