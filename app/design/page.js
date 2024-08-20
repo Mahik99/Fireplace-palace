f1754171739e3555a19454cf722aebe4620fc6d3
Mahik <mahik09@outlook.com>
2024-08-20 11:17:22 +0100

    added new design page and styled it

---
+++ app/design/page.js
@@ -0,0 +1,91 @@
+import Link from "next/link";
+import styles from "./design.module.css";
+
+export default function ContactForm() {
+  return (
+    <>
+      <h1 className={styles.formTiltle}>Design Booking</h1>
+      <form>
+        <fieldset className={styles.fieldset}>
+          <legend className={styles.formLegend}>Personal Information:</legend>
+          <ul className={styles.ul}>
+            <li className={styles.formList}>
+              <label className={styles.formLabel} htmlFor="name">
+                Full Name{" "}
+              </label>
+              <input
+                className={styles.inputBox}
+                type="text"
+                id="name"
+                name="user_name"
+              />
+            </li>
+            <li className={styles.formList}>
+              <label className={styles.formLabel} htmlFor="postcode">
+                Postcode{" "}
+              </label>
+              <input
+                className={styles.inputBox}
+                type="text"
+                id="postcode"
+                name="user_postcode"
+              />
+            </li>
+            <li className={styles.formList}>
+              <label className={styles.formLabel} htmlFor="address">
+                House/ Flat Number And Street Name
+              </label>
+              <input
+                className={styles.inputBox}
+                type="text"
+                id="address"
+                name="user_address"
+              />
+            </li>
+            <li className={styles.formList}>
+              <label className={styles.formLabel} htmlFor="city">
+                City{" "}
+              </label>
+              <input
+                className={styles.inputBox}
+                type="text"
+                id="city"
+                name="user_city"
+              />
+            </li>
+          </ul>
+        </fieldset>
+        <fieldset className={styles.fieldset}>
+          <legend className={styles.formLegend}>Contact Information:</legend>
+          <ul className={styles.ul}>
+            <li className={styles.formList}>
+              <label className={styles.formLabel} htmlFor="phone_number">
+                Phone Number{" "}
+              </label>
+              <input
+                className={styles.inputBox}
+                type="tel"
+                id="phone_number"
+                name="user_phone_number"
+              />
+            </li>
+            <li className={styles.formList}>
+              <label className={styles.formLabel} htmlFor="mail">
+                Email{" "}
+              </label>
+              <input
+                className={styles.inputBox}
+                type="email"
+                id="mail"
+                name="user_email"
+              />
+            </li>
+          </ul>
+        </fieldset>
+        <button className={styles.consultationButton}>
+          Request Design consultation
+        </button>
+      </form>
+    </>
+  );
+}
--- src/components/Hero/HeroDescription/HeroDescription.jsx
+++ src/components/Hero/HeroDescription/HeroDescription.jsx
@@ -1,18 +1,18 @@
-
-import styles from './HeroDescription.module.css'
+import Link from "next/link";
+import styles from "./HeroDescription.module.css";
 
 const HeroDescription = () => {
-    return (
-        <div className={styles.heroDescription}>
-        <p className={styles.heroHeadline}>
-          Discover the <br />
-          perfect fireplace ...
-        </p>
-        <p className={styles.consultationParagraph}>
-          Book consultation: <span className={styles.numberSpan}>0121 345 6789</span>
-        </p>
-      </div>
-    )
-}
+  return (
+    <div className={styles.heroDescription}>
+      <p className={styles.heroHeadline}>
+        Discover the <br />
+        perfect fireplace ...
+      </p>
+      <p className={styles.consultationParagraph}>
+        <Link href="/design">Request a design consultation</Link>
+      </p>
+    </div>
+  );
+};
 
-export default HeroDescription
+export default HeroDescription;
