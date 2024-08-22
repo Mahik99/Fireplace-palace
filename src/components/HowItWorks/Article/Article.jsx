07c420cf7719dc652348fa8b6429ff622f9a1a8b
Mahik <mahik09@outlook.com>
2024-08-22 14:13:23 +0100

    first test- page loading completed

--- src/components/HowItWorks/Article/Article.jsx
+++ src/components/HowItWorks/Article/Article.jsx
@@ -1,12 +1,18 @@
-import styles from './Article.module.css';
-import Image from 'next/image';
+import styles from "./Article.module.css";
+import Image from "next/image";
 
 const Article = ({ imageSrc, altText, title, description }) => {
   return (
     <article className={styles.tile}>
-      <Image src={imageSrc} alt={altText} className={styles.tileImage} width = "250" height="250" />
+      <Image
+        src={imageSrc}
+        alt={altText}
+        className={styles.tileImage}
+        width="249"
+        height="249"
+      />
       <h3>{title}</h3>
-      <p className='articleP'>{description}</p>
+      <p className="articleP">{description}</p>
     </article>
   );
 };
